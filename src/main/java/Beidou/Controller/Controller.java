407ee5238f17cf6dce09b7399de41e56cf443e36
cb <2916919961@qq.com>
2020-09-03 13:59:22 +0800

    modify one demo

--- src/main/java/Beidou/Controller/Controller.java
+++ src/main/java/Beidou/Controller/Controller.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
     @RequestMapping("route")
     public String getString(){
-       String a ="hello Beidou";
-        return a;
+
+        return "hello beidou";
     }
 }
